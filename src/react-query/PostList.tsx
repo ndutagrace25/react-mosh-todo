c508994b0678bd1f8fc6c73924b5f067d206d8ef
ndutagrace25 <ndutagrace25@gmail.com>
2023-11-08 14:14:23 +0300

    added pagination

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,20 +1,39 @@
 import axios from "axios";
 import usePosts from "./hooks/usePosts";
+import { useState } from "react";
 
 const PostList = () => {
-  const { data: posts, error, isLoading } = usePosts();
+  const pageSize = 10;
+  const [page, setPage] = useState<number>(1);
+
+  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="d-flex flex-row align-items-center my-3">
+        <button
+          disabled={page === 1}
+          className="btn btn-primary"
+          onClick={() => setPage(page - 1)}
+        >
+          Previous
+        </button>
+        <span className="mx-3">{page}</span>
+        <button className="btn btn-primary" onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
+    </>
   );
 };
 
--- src/react-query/hooks/usePosts.tsx
+++ src/react-query/hooks/usePosts.tsx
@@ -8,14 +8,25 @@ interface Post {
   userId: number;
 }
 
-const usePosts = () =>
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) =>
   useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    queryKey: ["posts", query],
     queryFn: () =>
       axios
-        .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+        .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (query.page - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
         .then((res) => res.data),
     staleTime: 1 * 60 * 1000, //1 min
+    keepPreviousData: true,
   });
 
 export default usePosts;
